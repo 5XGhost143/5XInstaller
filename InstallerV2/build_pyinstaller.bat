f8a40212246c4e19e3fb0e954919a92190c14a4e
5XGhost143 <144170441+5XGhost143@users.noreply.github.com>
2025-07-15 17:48:55 +0200

    Update build_pyinstaller.bat

--- InstallerV2/build_pyinstaller.bat
+++ InstallerV2/build_pyinstaller.bat
@@ -1,3 +1,4 @@
 @echo off
-pyinstaller --onefile --noconsole --icon=ico/installer.ico main.py
+pyinstaller --onefile --noconsole --icon=ico\installer.ico main.py
+copy ico\installer.ico dist\installer.ico
 pause
