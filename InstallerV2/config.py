72b6b4d0701b3f70bcdbf787ee740f4cdaf96ef9
5XGhost143 <144170441+5XGhost143@users.noreply.github.com>
2025-07-16 02:51:51 +0200

    Update config.py

--- InstallerV2/config.py
+++ InstallerV2/config.py
@@ -1,10 +1,11 @@
 # WINDOWS ONLY
 
 # Settings
-output_exe = "yourfilename.exe"
+output_exe = "FileName.exe"
 desktop_shortcut_option_show = True
 startmenu_shortcut_option_show = True
 add_to_path_option_show = True
+autostart_option_show = True
 
 App_Name = "Test"
 App_Desc = "Installer for the App Test"
